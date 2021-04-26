ee1d5dc1f16de5556bfcdb056e3cba6cfba604fb
achiever <tjddus6481@naver.com>
2021-04-26 22:27:55 +0900

    #11.8 Class Based Views part Two
    #11.8 Class Based Views part Two

--- rooms/views.py
+++ rooms/views.py
@@ -1,11 +1,21 @@
-from django.shortcuts import render
-from django.core.paginator import Paginator
+from django.views.generic import ListView
 from . import models
 
 
-def all_rooms(request):
-    page = request.GET.get("page")
-    room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10)
-    rooms = paginator.get_page(page)
-    return render(request, "rooms/home.html", context={"page": rooms})
+class HomeView(ListView):
+
+    """ HomeView Definition """
+
+    model = models.Room
+    paginate_by = 10
+    ordering = "created"
+
+# def all_rooms(request):
+#     page = request.GET.get("page")
+#     room_list = models.Room.objects.all()
+#     paginator = Paginator(room_list, 10)
+#     try:
+#         rooms = paginator.page(int(page))
+#         return render(request, "rooms/home.html", context={"page": rooms})
+#     except EmptyPage:
+#         return redirect
